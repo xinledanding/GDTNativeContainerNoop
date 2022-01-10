faa16c764bc77dfe5763d0228e82491d492f1335
le <le.xin@ximalaya.com>
2022-01-10 16:23:58 +0800

    [feat] 上传nativeAdContainer

---
+++ lib/src/main/java/com/qq/e/ads/nativ/widget/NativeAdContainer.java
@@ -0,0 +1,22 @@
+package com.qq.e.ads.nativ.widget;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+public class NativeAdContainer extends FrameLayout {
+
+    public NativeAdContainer(Context context) {
+        super(context);
+    }
+
+    public NativeAdContainer(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NativeAdContainer(Context context, AttributeSet attrs,
+                             int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
