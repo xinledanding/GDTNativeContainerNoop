187cb39a6625d5f9bceef1241fcc895f75b01913
le <le.xin@ximalaya.com>
2022-01-10 15:59:48 +0800

    Initial commit

---
+++ app/src/main/java/com/ximalaya/ting/android/gdtnativecontainernoop/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ximalaya.ting.android.gdtnativecontainernoop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
